1a09e8ce4bdcca01112443e2f5050ebbd15433d3
Charper <55903458+Charper@users.noreply.github.com>
2022-04-12 17:00:15 -0500

    initial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Contact } from './contact';
+import { contacts } from './sample-contacts';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'contact-manager';
+
+  contacts: Contact[] = contacts;
+  
 }
---
+++ src/app/sample-contacts.ts
@@ -0,0 +1,7 @@
+import { Contact } from './contact';
+
+export const contacts: any = [
+    { fName: 'John', lName: 'Harmon', phone: 1112223333, email: 'john.harmon@gmail.com' },
+    { fName: 'Grant', lName: 'Fleming', phone: 4445556666, email: 'grant.fleming@gmail.com' },
+    { fName: 'Sarah', lName: 'Smith', phone: 7778889999, email: 'sarah.smith@gmail.com'}
+]
